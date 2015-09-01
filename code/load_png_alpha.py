c0285c0879101fe05a49fba0ccdc5400b3a98415
David Liebman <radiodee1@gmail.com>
2015-09-01 15:41:28 -0400

    check if training progress is made

--- code/load_png_alpha.py
+++ code/load_png_alpha.py
@@ -1,12 +1,286 @@
+import numpy
+#import pylab
+from PIL import Image
+import cPickle
+import gzip
+import os
+import sys
+import time
+import random
 
+import math
+from glob    import glob
+from os.path import join
+from os.path import expanduser
+import getpass
+#import pickle_local as plocal
+#import mnist_loader as mnistl
 import enum_local as LOAD
 
 '''
-Note: this method is duplicated in the cnn_both.py file.
+def get_dataset(alphabet_set = True, 
+        test_only = False, 
+        img_pickle_ld = False, 
+        img_pickle_sv = False, 
+        nist_stretch = 2,
+        large_size = -1 ,
+        save_filename = "",
+        randomize = True,
+        load_type = LOAD.ALPHANUMERIC) :
+        
+    epochs = 10
+    if (save_filename != "") :
+        nist_stretch, large_size, epochs, save_filename , load , load_type = parse_filename(save_filename)
+        if load :
+            img_pickle_ld = True
+        else :
+            img_pickle_sv = False
+        
+    nist_num = nist_stretch  *  1024 * 5 ## TOTAL IMG SPACE REQUESTED (images)
+    if (large_size == -1) :
+        large_size = 1024 * 10 * (28 * 28)  ## LIMIT SIZE (not images)
+    else:
+        size = 28, 28
+        img2 = numpy.zeros(shape=(size), dtype='float32')
+        s = sys.getsizeof(img2)
+        print(str(s) + " size of 28x28")
+        if (large_size > s) : large_size = large_size /  (s) ## (convert to num of images.)
+        else : print ("small 'BigBatch' size, using as num of images.")
+    rval_array = []
+    pickle_len = 0
+    obj = [[],0]
+    dset = [[obj,obj,obj]]
+    print("images requested: " + str(nist_num))
+
+    t1 = []
+    l1 = []
+    t2 = []
+    l2 = []
+    t3 = []
+    l3 = []
+
+    if img_pickle_ld and (not img_pickle_sv) and load_type != LOAD.NUMERIC :
+        dset = plocal.load_pickle_img(save_filename);
+        #for j in dset :
+        pickle_len =  len(dset[0][0][0] )
+        
+        pickle_len = int(pickle_len  )
+        print(pickle_len, nist_num)
+        if pickle_len < nist_num : nist_num = pickle_len
+    
+    if nist_num > large_size:
+        print("Automatically splitting input files.")
+        m = int(math.ceil(nist_num / float(large_size)))
+    else:
+        m = 1
+    if  (not img_pickle_sv) :
+        
+        for zz in range(m):
+            print('get dataset. ' + str(zz+1) + ' of ' + str(m))
+            
+            if (not alphabet_set) or load_type == LOAD.NUMERIC   :
+                
+                print "load numeric"
+            if alphabet_set and (not load_type == LOAD.NUMERIC) :
+                
+                stretch =  int(nist_num / float(10)) 
+                
+                if nist_num > large_size :
+                    stretch = int(large_size / float(10) )
+                    #print('middle')
+                
+                if nist_num > large_size and zz == m -1 : 
+                    stretch = int (((nist_num ) - (zz * large_size  ))/ float(10))  
+                    #print('end')
+                
+                if  True : ##not img_pickle_sv : ## not nist_num == pickle_len and
+                
+                    train_start = zz * large_size; # start with zero
+                    train_stop =  train_start + (stretch * 8);
+                    valid_start = train_stop + 1;
+                    valid_stop =  valid_start + (stretch * 1);
+                    test_start =  valid_stop + 1;
+                    test_stop =   test_start + (stretch * 1);
+
+                    
+                    if (not img_pickle_ld) and load_type != LOAD.NUMERIC :
+                        if not test_only :
+                            files = []
+                            t1, l1, files = batch_load_alpha( train_start,  train_stop, randomize, files, load_type)
+                            t2, l2, files = batch_load_alpha( valid_start,  valid_stop, randomize, files, load_type)
+                            
+                        else:
+                            t1 = l1 = t2 = l2 = files = []
+                            
+                        t3, l3 , files = batch_load_alpha( test_start,   test_stop, randomize, files, load_type)
+                    
+                    
+                    elif img_pickle_ld :
+                        print('Loading pickle file: ~/workspace/nn/' + save_filename )
+                        print('delete this file to disable loading.')
+                        
+                        dset2 = dset[0][0]
+                        #print dset2[1]
+                        
+                        t1, l1 = dset2[0][train_start:train_stop] , dset2[1][train_start:train_stop]
+                        t2, l2 = dset2[0][valid_start:valid_stop] , dset2[1][valid_start:valid_stop]
+                        t3, l3 = dset2[0][test_start:test_stop]   , dset2[1][test_start:test_stop]
+                        print('train: ' + str(len(l1)))
+                        print('valid: ' + str(len(l2)))
+                        print('test:  ' + str(len(l3)))
+                        
+                    
+            rval = [(t1, l1), (t2, l2), (t3, l3)]
+
+
+            rval_array.append( rval )
+        
+    elif (not test_only)  and img_pickle_sv :
+        num_start = 0
+        num_stop = nist_num
+        t1, l1 , files = batch_load_alpha( num_start,  num_stop, randomize=False, files = [], load_type=load_type)
+        print('train: ' + str(len(l1)))
+        
+        #plocal.save_pickle_img( rval_array ,[],[], filename = save_filename );
+        plocal.save_pickle_img(  [[[t1, l1]]]  ,[],[], filename = save_filename );
+        print('\nImage Pickle Save: only works for small image sets!')
+        print('(it hogs memory and will freeze your computer.)')
+    return rval_array[0][0] #, epochs, nist_stretch, load_type
 '''
 
+	
+def filename_list(series_start=0, series_stop=2, randomize = False , files = [] , load_type = 0):
+    
+    if len(files) == 0 :
+        files = []
+        folder = 'F*'
+        #folder_username = getpass.getuser()
+        home = expanduser("~")
+        
+        #print(folder)
+        g = glob(join(home ,'workspace','sd_nineteen','HSF_0',folder))
+        h = glob(join(home ,'workspace','sd_nineteen','HSF_1',folder))
+        g.extend(h)
+        i = glob(join(home ,'workspace','sd_nineteen','HSF_2',folder))
+        g.extend(i)
+        jj = glob(join(home ,'workspace','sd_nineteen','HSF_3',folder))
+        g.extend(jj)
+        kk = glob(join(home ,'workspace','sd_nineteen','HSF_4',folder))
+        g.extend(kk)
+        ll = glob(join(home ,'workspace','sd_nineteen','HSF_6',folder))
+        g.extend(ll)
+        mm = glob(join(home ,'workspace','sd_nineteen','HSF_7',folder))
+        g.extend(mm)
+        g.sort()
+        
+        #print ("sorted folder list: ", len(g))
+        for j in g : #range(series_start, series_stop):
+            gg = glob(join( j ,'*.bmp'))
+            #print ("list: ",gg)
+            files.extend(gg)
+        print ('loadable files: '+ str(len(files)))
+        print ('loaded files  : ' + str(int(series_stop - series_start)))
+        files.sort()
+    output = []
+    if not randomize :
+        output = files[int(series_start): int(series_stop) ]
+    else :
+        print len(files)
+        num_files = int( series_stop - series_start )
+        for j in range(num_files) :
+            digit_start = 48
+            k = random.randint(0, len(files))
+            xxx, d = get_number(files[k], load_type)
 
-def ascii_ymatrix(alphabet_set=LOAD.ALPHA ) :
+            while d >= digit_start and d < digit_start + 10 and load_type == LOAD.ALPHA :
+                #print d - digit_start
+                del files[k]
+                k = random.randint(0, len(files))
+                xxx, d = get_number(files[k], load_type)
+
+            xxx, d = get_number(files[k], load_type)
+            if d >= digit_start and d < digit_start + 10 and load_type == LOAD.ALPHA:
+                print "file error number detected"
+                exit()
+            #if j is 0 : print files[k]
+            output.append(files[k])
+            del files[k]
+            
+    return output, files
+    
+	
+def batch_load_alpha(series_start = 1, series_stop = 1, randomize = False, files = [], load_type =0):
+    img_list , files = filename_list(series_start, series_stop, randomize, files, load_type )
+    train_set = []
+    train_num = []
+    oneimg = []
+    oneindex = 0
+    i = 0
+    if (len(img_list) > 0) and True:
+        print('sample: ' + img_list[0])
+        sys.stdout.flush()
+    
+    for filename in img_list:
+
+        oneimg, oneindex = look_at_img(filename, load_type=load_type)
+        train_set.append(oneimg)
+        train_num.append(oneindex)
+        #print(filename)
+    return train_set, train_num, files
+
+def look_at_img( filename , i = 0, load_type =0):
+    img = Image.open(open( filename ))
+    size = 28, 28
+    img2 = numpy.zeros(shape=(size), dtype='float64')
+    oneimg = []
+    oneindex = i
+    xy_list = []
+    
+    img = numpy.asarray(img, dtype='float64')
+    marker = 0
+    ''' Detect 0 for black -- put in list in shrunk form. '''
+    for x in range(0,len(img)):
+        for y in range(0, len(img)):
+            if (float(img[x,y,0]) < 255) is  True:
+                xy_list.append([x* 1/float(2) - 18,y * 1/float(2) - 18])
+    
+    ''' Put list in 28 x 28 array. '''
+    if len(xy_list) == 0 :
+        xy_list = [0,0]
+    for q in xy_list :
+        if (q[0] < 28) and (q[1] < 28) and (q[0] >= 0) and (q[1] >= 0):
+            #print (q[0], q[1])
+            img2[int(math.floor(q[0])), int(math.floor(q[1]))] = 1
+    
+    ''' Then add entire array to oneimg variable and flatten.'''
+    for x in range(28) :
+        for y in range(28) :
+            oneimg.append(img2[x,y])
+    
+    ''' Get the image ascii number from the filename. '''
+    oneindex , unused = get_number(filename, load_type)
+    return oneimg, oneindex
+
+def get_number(filename, load_type ):
+    mat = ascii_ymatrix(load_type)
+    newindex = 0
+    index = 0
+    l_bmp = len('.bmp')  ## discard this many chars for ending
+    l_sample = l_bmp + 2 ## sample two chars
+    
+    l_filename = len(filename)
+    filename = filename[l_filename - l_sample : l_filename - l_bmp] ## slice
+    if filename[0:1] is '_':
+        filename = filename[1: len(filename)] ## slice again
+        ## consume first char.
+    filename = '0x' + filename
+    index = int(filename, 16) ## translate hex to int
+    for i in range(len(mat)) :
+        if index is mat[i][0] :
+            newindex = i
+    return newindex, index
+
+def ascii_ymatrix(alphabet_set=LOAD.ALPHANUMERIC ) :
     mat = []
     a_upper = 65 ## ascii for 'A'
     a_lower = 97 ## ascii for 'a'
