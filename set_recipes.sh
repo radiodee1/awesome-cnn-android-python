6c33ec9fd93eb974d95c9141ff35b0f2d3c7d2b5
David Liebman <radiodee1@gmail.com>
2015-09-01 16:34:14 -0400

    save set_recipes.sh file

--- set_recipes.sh
+++ set_recipes.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#cp -R recipes/*  ~/workspace/python-for-android/recipes/.
 
 BUILDOZERDIR=`pwd`/
 CODEDIR=`pwd`/code/
@@ -10,6 +9,8 @@ RESDIR=`pwd`/res
 
 PY4ADIR=~/workspace/python-for-android/
 
+#cp -R $BUILDOZERDIR/recipes/* $PY4ADIR/recipes/.
+
 PROJECTDIR=`pwd`/GetText/GetText/src/main/java/
 
 FILENAME=org/renpy/android/GetText.java
@@ -18,11 +19,12 @@ cd $JAVADIR
 #javac *.java
 cd $BUILDOZERDIR
 
-mkdir -p $RESDIR
+mkdir -p $RESDIR 
+mkdir -p $PY4ADIR/src/res/raw/
 
 cd $PY4ADIR/src/res/
 mkdir -p raw
-cp -R $RESDIR/* $PY4ADIR/src/res/raw/.
+cp -pR $RESDIR/* $PY4ADIR/src/res/raw/.
 
 cp -pR $PROJECTDIR/$FILENAME $PY4ADIR/src/src/$FILENAME
 
