e1c522c68b2665068692bf7e94199d43f035caf2
David Liebman <radiodee1@gmail.com>
2015-09-26 13:58:22 -0400

    rm load_png_alpha.py

--- recipes/awesomecnn/recipe.sh
+++ recipes/awesomecnn/recipe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION_awesomecnn=0.23
+VERSION_awesomecnn=0.24
 URL_awesomecnn=https://github.com/radiodee1/awesome-cnn/archive/v$VERSION_awesomecnn.zip
 
 DEPS_awesomecnn=(python numpy pyjnius)
